11243ef1d6069cbeb8647f2fff9ba98cd3ffeb7b
Alec Asgari <asgarialec@gmail.com>
2025-05-08 15:48:23 +0400

    Header and Footer added

---
+++ header.php
@@ -0,0 +1,15 @@
+<head>
+    <!-- Meta Tags -->
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="author" content="Thememarch" />
+    <!-- Favicon Icon -->
+    <link rel="icon" href="assets/img/favicon.svg" />
+    <!-- Site Title -->
+    <title>Vixan - Creative Agency Template</title>
+    <link rel="stylesheet" href="assets/css/plugins/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/css/plugins/lightgallery.min.css" />
+    <link rel="stylesheet" href="assets/css/plugins/swiper.min.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
+</head>
